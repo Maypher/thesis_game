8da5d64cb284ad93ed030f5833c73cb068b29158
Maypher <maypherOffcial@gmail.com>
2023-04-01 17:33:18 -0400

    Add new function that returns true if any collider in range

--- Assets/Scripts/Helpers/AttackCheck.cs
+++ Assets/Scripts/Helpers/AttackCheck.cs
@@ -7,8 +7,18 @@ public class AttackCheck : MonoBehaviour
     [SerializeField] private float _checkRadius = .5f;
     [SerializeField] private LayerMask _whatIsEnemy;
 
-    public Collider2D[] Check()
+    public Collider2D[] GetEnemies()
     {
         return Physics2D.OverlapCircleAll(transform.position, _checkRadius, _whatIsEnemy);
+    }
+
+    public bool Check()
+    {
+        return Physics2D.OverlapCircleAll(transform.position, _checkRadius, _whatIsEnemy).Length != 0;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, _checkRadius);
     }
 }
