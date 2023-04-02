25198aa8cc3d2e074c0c343bc7489691bd9a644e
Maypher <maypherOffcial@gmail.com>
2023-04-02 12:27:27 -0400

    Add event that gets called onTriggerEnter

--- Assets/Scripts/Helpers/AttackCheck.cs
+++ Assets/Scripts/Helpers/AttackCheck.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class AttackCheck : MonoBehaviour
 {
     [SerializeField] private float _checkRadius = .5f;
     [SerializeField] private LayerMask _whatIsEnemy;
+
+    [HideInInspector] public Action enemyEnteredAttackArea;
 
     public Collider2D[] GetEnemies()
     {
@@ -15,6 +19,11 @@ public class AttackCheck : MonoBehaviour
     public bool Check()
     {
         return Physics2D.OverlapCircleAll(transform.position, _checkRadius, _whatIsEnemy).Length != 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == _whatIsEnemy) enemyEnteredAttackArea?.Invoke();
     }
 
     private void OnDrawGizmos()
