a968d7e35a9a4bc53ebc0d7fa1e1e1ca43fd38b0
Maypher <maypherOffcial@gmail.com>
2023-06-11 13:47:38 -0400

    Add parallax background effect

---
+++ Assets/Scripts/Enviroment/Background.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private Vector2 movementSpeed;
+    [SerializeField] private float playerVelocityMultiplier = .1f;
+
+    private Material material;
+
+    private Rigidbody2D playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        playerRb = GameManager.Player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = (playerRb.velocity.x * playerVelocityMultiplier) * Time.deltaTime * movementSpeed;
+        material.mainTextureOffset += offset;
+    }
+}
